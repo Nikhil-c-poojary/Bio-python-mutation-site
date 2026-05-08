259789b31c24556a2288a4a14c80b0716faec210
Nikhil. C <nikhilvcp26@gmail.com>
2026-05-08 10:31:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-                if a == b:
                     alignment_visual.append("|")
                 else:
                     alignment_visual.append(" ")
